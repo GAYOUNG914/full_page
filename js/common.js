0b9a17344d3c83e645c3ca6ec9d68bc8c39e9b6b
김가영 Kim gayoung <77196388+GAYOUNG914@users.noreply.github.com>
2021-08-23 18:58:49 +0900

    docs: add code
    add code

--- js/common.js
+++ js/common.js
@@ -1,35 +1,53 @@
-$(function() {
+$(function () {
   let currentIndex = 0,
-      oldIndex;
-  $(window).on("mousewheel",function(e){
-    
+    oldIndex = 0;
+  $(window).on("mousewheel", function (e) {
     let d = e.originalEvent.wheelDelta;
-    // let divTopN = $('div').next().offset().top;
-    // let divTopP = $('div').prev().offset().top;
-    // $('html, body').animate({scrollTop: divTop}, 1000);
 
-    if( d > 0) {
-      $('.wrap div').eq(currentIndex).animate({top: '100%'}, 1000);
+    if (d > 0) {
+      // console.log($(".wrap div:first").index());
+      if (oldIndex === 0) {
+        return;
+      }
+      if ($(".wrap div").is(":animated")) {
+        $(".wrap div").animate();
+        return;
+      }
+      console.log(currentIndex);
+      $(".wrap div").eq(currentIndex).stop().animate({ top: "100%" }, 1000);
       oldIndex = --currentIndex;
-      // oldIndex = currentIndex
-      console.log(oldIndex)
-      $('.wrap div').eq(oldIndex).animate({top: '0'}, 1000);
+      $(".wrap div").eq(oldIndex).stop().animate({ top: "0" }, 1000);
+      console.log(currentIndex);
+
+      $("ul li").on("click", function () {
+        //클릭 된 div만 끌어올리기
+        $(".wrap div").eq(currentIndex).stop().animate({ top: "100%" }, 1000);
+        $(".wrap div").eq($(this).index()).stop().animate({ top: "0" }, 1000);
+      });
     } else {
-      // console.log('down')
+      if (oldIndex === 3) {
+        return;
+      }
 
-      $('.wrap div').eq(currentIndex).animate({top: '-100%'}, 1000);
+      if ($(".wrap div").is(":animated")) {
+        $(".wrap div").animate();
+        return;
+      }
+      // console.log($(".wrap div").is(":animated"));
+      $(".wrap div").eq(currentIndex).stop().animate({ top: "-100%" }, 1000);
       oldIndex = ++currentIndex;
-      // oldIndex = currentIndex
-      console.log(oldIndex)
-      $('.wrap div').eq(oldIndex).animate({top: '0'}, 1000);
-    };
-  });
+      $(".wrap div").eq(oldIndex).stop().animate({ top: "0" }, 1000);
 
+      $("ul li").on("click", function () {
+        //클릭 된 div만 끌어올리기
+        $(".wrap div").eq(currentIndex).stop().animate({ top: "-100%" }, 1000);
+        $(".wrap div").eq($(this).index()).stop().animate({ top: "0" }, 1000);
+      });
+    }
+  });
 
   // 스크롤 up => -100% -> 0 되어야함/ 0 -> 100% 되어야함
   // 스크롤 down =>100% -> 0 / 0 -> -100%
 
   // 마우스휠 동작할 때 애니메이션 걸려야함
-
-
 });
